7a32d9e0c68cb4b09dc8472bcfb8f7c4fad7f9e8
yambal <jy@yambal.net>
2016-07-11 11:09:09 +0900

    Cherome Login #1

--- platforms/chrome/background.js
+++ platforms/chrome/background.js
@@ -2,7 +2,7 @@
  * Create a new Window for the app
  */
 chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('www/index.html', {
+  chrome.app.window.create('www/ChromeAppLogin.html', {
     bounds: {
       width: 480,
       height: 640,
